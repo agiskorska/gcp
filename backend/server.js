e003b9ae2f6cd71ca9f77cda3841194113e52115
agiskorska <aggie.skorska2@kingfisher.com>
2024-10-18 11:53:01 +0100

    Pushed to gcp

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,11 @@ const app = express();
 const dbFile = "db.json";
 const PORT = process.env.PORT || 8080;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// POST endpoint to save data to the db.json file
 app.post("/save", (req, res) => {
   const data = req.body;
 
-  // Read the current contents of db.json (if any)
   fs.readFile(dbFile, "utf8", (err, fileData) => {
     if (err && err.code !== "ENOENT") {
       return res
@@ -40,7 +37,7 @@ app.post("/save", (req, res) => {
 });
 
 // GET endpoint to retrieve data from the db.json file
-app.get("/data", (req, res) => {
+app.get("/", (req, res) => {
   fs.readFile(dbFile, "utf8", (err, fileData) => {
     if (err && err.code === "ENOENT") {
       return res.status(200).json({ message: "No data found" });
